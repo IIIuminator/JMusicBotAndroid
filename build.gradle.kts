b2f8df46abbe60647d8713be53aef1452a54f8c4
Ivo Berger <ivo-berger@outlook.com>
2019-08-10 15:31:14 +0200

    dep upgrades

--- build.gradle.kts
+++ build.gradle.kts
@@ -30,23 +30,7 @@ spotless {
     kotlin {
         target("**/kotlin/**/*.kt")
         ktlint()
-        licenseHeader(
-            "/*\n" +
-                "* Copyright 2019 Ivo Berger\n" +
-                "*\n" +
-                "* Licensed under the Apache License, Version 2.0 (the \"License\");\n" +
-                "* you may not use this file except in compliance with the License.\n" +
-                "* You may obtain a copy of the License at\n" +
-                "*\n" +
-                "* http://www.apache.org/licenses/LICENSE-2.0\n" +
-                "*\n" +
-                "* Unless required by applicable law or agreed to in writing, software\n" +
-                "* distributed under the License is distributed on an \"AS IS\" BASIS,\n" +
-                "* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.\n" +
-                "* See the License for the specific language governing permissions and\n" +
-                "* limitations under the License.\n" +
-                "*/"
-        )
+        licenseHeaderFile("licenseHeader.txt")
     }
     kotlinGradle {
         target("**/*.gradle.kts")
@@ -56,5 +40,5 @@ spotless {
 
 tasks.wrapper {
     distributionType = Wrapper.DistributionType.BIN
-    version = Versions.Gradle.runningVersion
+    version = Versions.gradleLatestVersion
 }
